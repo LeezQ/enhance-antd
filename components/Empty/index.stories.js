22c45bca0b7711fec0687ca3c7c99d54f3d50f9c
LeezQ <lizhenq2009@gmail.com>
2019-04-25 20:35:30 +0800

    mv js -> tsx

--- components/Empty/index.stories.js
+++ components/Empty/index.stories.js
@@ -1,16 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button } from 'antd';
-
 import { storiesOf } from '@storybook/react';
-
 import Empty from './index';
-
-storiesOf('Empty', module).add('空白演示', () => (
-  <Empty
-    extra={
-      <>
-        暂无数据 <Button type="primary">创建</Button>
-      </>
-    }
-  />
-));
+storiesOf('Empty', module).add('空白演示', () => (React.createElement(Empty, { extra: React.createElement(React.Fragment, null,
+        "\u6682\u65E0\u6570\u636E ",
+        React.createElement(Button, { type: "primary" }, "\u521B\u5EFA")) })));
